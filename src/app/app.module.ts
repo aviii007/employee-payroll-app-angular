feab2d9e660b57d32dcb97b77be094256979b7da
aviii007 <kanojia.avi@gmail.com>
2023-02-20 16:36:26 +0530

    Added to display the home page with add button will route to employe payroll form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormComponent } from './components/form/form.component';
 import { HomeComponent } from './components/home/home.component';
-
+import { FormComponent } from './components/form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent,
     HomeComponent,
-    
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
